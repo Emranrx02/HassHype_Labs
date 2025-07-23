b426af48665880e5343eaaf98decf6996d899204
Emran Haque <emran.huk2016@gmail.com>
2025-07-24 03:46:18 +0600

    Update a tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="hero-section">
       <div className="hero-content">
-        <h1><span>WEB 3.0</span> IMPACT ON FUTURE WITH US</h1>
+        <h1><span>WEB 3.0</span> Your Dream Build with us </h1>
         <p>Powered by Emran’s Team, we don’t just manage — we build, grow, and maintain Web3 communities that thrive.
 From viral strategies to rock-solid support, your mission becomes ours.</p>
         <a href="https://t.me/emranrx" target="_blank" rel="noopener noreferrer">
