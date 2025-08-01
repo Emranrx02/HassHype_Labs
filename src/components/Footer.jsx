a6f0f9635db1466160b54d5efdd3b670bd50b71b
Emran Haque <emran.huk2016@gmail.com>
2025-08-01 12:02:13 +0600

    update new client reivew section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 HashHype Labs. All rights reserved. Developer <a href="https://www.emranhaque.com" target="_blank" rel="noopener noreferrer">Emran Haque</a> for updates.</p>
+      <p>©2025 HashHype Labs. All rights reserved. Developer <a href="https://www.emranhaque.com" target="_blank" rel="noopener noreferrer">Emran Haque</a></p>
     </footer>
   );
 }
